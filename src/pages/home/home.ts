b9e827fee14a7062fb117ea947fc04143a51b149
trainee 3 <trainee3@mitrais.com>
2016-12-08 13:23:22 +0800

    first tutor

---
+++ src/pages/home/home.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {Camera} from 'ionic-native';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+public base64Image: string;
+
+  constructor() {}
+  takePicture(){
+    Camera.getPicture({
+        destinationType: Camera.DestinationType.DATA_URL,
+        targetWidth: 1000,
+        targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
+    });
+    }
+}
